72a70563193678c552971e9fd042c2248651d24a
Samer Ghaly <samereaghaly@gmail.com>
2023-04-01 22:02:59 +0200

    created basic course card component

---
+++ src/Components/Course.jsx
@@ -0,0 +1,11 @@
+function Course(){
+    return(
+        <div className="course-card">
+            <h3 className="course-name">Course Name</h3>
+            <p className="course-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est itaque sequi fugit accusantium suscipit velit mollitia officiis voluptatum.</p>
+            <ProgressBar/>
+        </div>
+    )
+}
+
+export default Course
