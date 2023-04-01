38d924835e89e6e8a90030c90e16eb0fee5dcf2a
Samer Ghaly <samereaghaly@gmail.com>
2023-04-01 22:03:23 +0200

    created basic progress bar component

---
+++ src/Components/ProgressBar.jsx
@@ -0,0 +1,10 @@
+function ProgressBar(){
+    return(
+        <div className="progress">
+            <div className="progress-bar">
+                <div className="progress-bar-completed"></div>
+            </div>
+            <p className="percentage"></p>
+        </div>
+    )
+}
