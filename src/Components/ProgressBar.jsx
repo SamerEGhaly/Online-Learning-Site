94073a61319903d822705830fc99225555ae5bac
Samer Ghaly <samereaghaly@gmail.com>
2023-04-01 22:32:57 +0200

    add styling to progress bar

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -1,10 +1,20 @@
+import "../CSS/progressBar.css"
+
 function ProgressBar(){
+
+    const progress = 70;
+    const progressStyle = {
+        backgroundColor: "#306EE5",
+        width: `${progress}%`,
+        height: "100%"
+    }
+
     return(
         <div className="progress">
             <div className="progress-bar">
-                <div className="progress-bar-completed"></div>
+                <div style={progressStyle} className="progress-bar-completed"></div>
             </div>
-            <p className="percentage"></p>
+            <p className="percentage">{`${progress}%`}</p>
         </div>
     )
 }
