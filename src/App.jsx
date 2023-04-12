d9139c994d3c5cf159a1cfa84bb826541cc9d807
Samer Ghaly <samereaghaly@gmail.com>
2023-04-12 18:39:25 +0200

    added landing page styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
 import React from "react"
 import Course from "./Components/Course"
 import Header from "./Components/Header"
+import LandingPage from "./Components/LandingPage"
 
 function App() {
-
-  const [searchText, setSearchText] = React.useState("")
-
-  function handleSearchInput(event){
-    const name = event.target.name
-    const value = event.target.value
-
-    if(name == "searchBox"){
-      setSearchText(value)
-      return true
-    }
-    return false
-  }
 
   return (
     <div className="main-container">
       <Header/>
       <main>
-        <section className="landing-page">
-          <input onChange={handleSearchInput} type="text" className="search-box" name="searchBox" placeholder="Search for courses" input={searchText}/>
-          <p>Explore what professionals like you are learning the most</p>
-          <button className="visitCourses">Visit Courses</button>
-        </section>
+        <LandingPage/>
       </main>
     </div>
   )
---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import "/src/CSS/landing-page.css"
+
+function LandingPage(){
+
+    const [searchText, setSearchText] = React.useState("")
+
+    function handleSearchInput(event){
+        const name = event.target.name
+        const value = event.target.value
+
+        if(name == "searchBox"){
+        setSearchText(value)
+        return true
+        }
+        return false
+    }
+
+    return(
+        <section className="landing-page">
+            <img src="\src\assets\Images\landing-page-background.png" alt="girl sitting infront of laptop background" className="landing-page-background" />
+          <input onChange={handleSearchInput} type="text" className="search-box" name="searchBox" placeholder="Search for courses" input={searchText}/>
+          <p>Explore what professionals like you are learning the most</p>
+          <button className="visitCourses">Visit Courses</button>
+        </section>
+    )
+}
+
+export default LandingPage
