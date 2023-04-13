596197cd32d58c4ef4a36d449c1162f14c32bc3a
Samer Ghaly <samereaghaly@gmail.com>
2023-04-13 15:30:40 +0200

    added online platforms section with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ function App() {
       <Header/>
       <main>
         <LandingPage/>
+        <section className="online-courses-platforms">
+          <img src="/src/assets/Images/udemy-logo.png" alt="udemy logo" />
+          <img src="/src/assets/Images/khan-academy-logo.png" alt="khan academy logo" />
+          <img src="/src/assets/Images/cloud-academy-logo.png" alt="cloud academy logo" />
+          <img src="/src/assets/Images/coursera-logo.png" alt="coursera logo" />
+        </section>
       </main>
     </div>
   )
