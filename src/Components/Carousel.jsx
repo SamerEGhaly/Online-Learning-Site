a5995b16b6d5c18d02940f64d5faa92cd98e87af
Samer Ghaly <samereaghaly@gmail.com>
2023-04-24 16:50:12 +0200

    implemented the course card

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,19 +1,14 @@
+import Course from "./Course"
 import "/src/CSS/carousel.css"
 
 function Carousel(){
     return(
         <div className="carousel">
             <div className="container">
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
+                <Course/>
+                <Course/>
+                <Course/>
+                <Course/>
             </div>
         </div>
     )
--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -2,7 +2,22 @@ import "../CSS/course.css"
 
 function Course(){
     return(
-        <div className="course-card"></div>
+        <div className="course-card">
+            <img src="/src/assets/Images/black_289x210.png" alt="course thumbnail" className="course-thumbnail" />
+            <div className="course-details">
+                <p className="course-tag">Popular</p>
+                <h2 className="course-title">Course Title</h2>
+                <p className="course-duration">Duration : 25mins</p>
+                <p className="course-price">$15.90</p>
+                <img src="/src/assets/Images/cloud-academy-logo.png" alt="platform logo" className="course-platform" />
+                <div className="rating">
+                    <div className="stars"></div>
+                    <p className="reviews-count">(400)</p>
+                </div>
+            </div>
+                <button className="enroll-button">Enroll Now</button>
+            <a href="#" className="more-info-link">View More Info</a>
+        </div>
     )
 }
 
