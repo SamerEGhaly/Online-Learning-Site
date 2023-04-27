e1a3f6984c9dff02610e6a00957df27834c31d42
Samer Ghaly <samereaghaly@gmail.com>
2023-04-28 01:47:46 +0300

    created instructed courses component

---
+++ src/Components/InstructedCourses.jsx
@@ -0,0 +1,14 @@
+function InstructedCourses(){
+    return(
+        <section className="instructed-courses">
+            <h2 id="instructed-courses-title">Instructed Courses</h2>
+            <p>Want someone to instruct you? No worries, here we introduce our CourseMania’s online Tutors  to assist & guide you in your professional  Path</p>
+            <button className="find-tutor-button">Find A Tutor</button>
+            <div className="popular-tutors">
+                <h3 id="popular-tutors-title">Meet Our Popular Tutors</h3>
+            </div>
+        </section>
+    )
+}
+
+export default InstructedCourses
