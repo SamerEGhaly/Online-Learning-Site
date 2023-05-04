1b8324db584692242d92eeb8f516d0d396eb84fe
Samer Ghaly <samereaghaly@gmail.com>
2023-05-04 20:51:58 +0300

    added json data files

---
+++ src/Data/courseData.js
@@ -0,0 +1,70 @@
+export default {
+    "courseData" : [
+        {
+            id: 0,
+            title: "unconcious bias",
+            duration: "25mins",
+            price: "free",
+            platform: "cloud academy",
+            stars: "4.5",
+            no_of_ratings: "43435",
+            tag: "popular",
+            thumbnail: "/src/assets/Images/black_289x210.png"
+        },
+        {
+            id: 1,
+            title: "communication",
+            duration: "50mins",
+            price: "15.90",
+            platform: "khan academy",
+            stars: "5",
+            no_of_ratings: "30435",
+            tag: "best seller",
+            thumbnail: "/src/assets/Images/black_289x210.png"
+        },
+        {
+            id: 2,
+            title: "critical thinking",
+            duration: "1hr 12mins",
+            price: "10.90",
+            platform: "khan academy",
+            stars: "5",
+            no_of_ratings: "28435",
+            tag: "best seller",
+            thumbnail: "/src/assets/Images/black_289x210.png"
+        },
+        {
+            id: 3,
+            title: "team work",
+            duration: "25mins",
+            price: "19.90",
+            platform: "cloud academy",
+            stars: "4.5",
+            no_of_ratings: "24100",
+            tag: "best seller",
+            thumbnail: "/src/assets/Images/black_289x210.png"
+        },
+        {
+            id: 4,
+            title: "leadership skills",
+            duration: "50mins",
+            price: "18.90",
+            platform: "khan academy",
+            stars: "5",
+            no_of_ratings: "18335",
+            tag: "best seller",
+            thumbnail: "/src/assets/Images/black_289x210.png"
+        },
+        {
+            id: 5,
+            title: "statistics",
+            duration: "1hr 12mins",
+            price: "10.90",
+            platform: "khan academy",
+            stars: "5",
+            no_of_ratings: "12434",
+            tag: "best seller",
+            thumbnail: "/src/assets/Images/black_289x210.png"
+        }
+    ]
+}
---
+++ src/Data/instructorsData.js
@@ -0,0 +1,40 @@
+export default {
+    "instructorData": [
+        {
+            id: 0,
+            name: "robert james",
+            specialization: "UI/UX  Designer",
+            no_of_courses: "56",
+            stars: "4.9",
+            no_of_ratings: "76335",
+            avatar: "/src/assets/Images/tutor-avatar.png"
+        },
+        {
+            id: 1,
+            name: "jessica thomas",
+            specialization: "Graphic Designer",
+            no_of_courses: "62",
+            stars: "4.9",
+            no_of_ratings: "87532",
+            avatar: "/src/assets/Images/tutor-avatar.png"
+        },
+        {
+            id: 2,
+            name: "selena mathew",
+            specialization: "Full Stack Developer",
+            no_of_courses: "37",
+            stars: "4.8",
+            no_of_ratings: "68868",
+            avatar: "/src/assets/Images/tutor-avatar.png"
+        },
+        {
+            id: 3,
+            name: "tom henry",
+            specialization: "SQL, Tableu",
+            no_of_courses: "42",
+            stars: "4.8",
+            no_of_ratings: "88973",
+            avatar: "/src/assets/Images/tutor-avatar.png"
+        }
+    ]
+}
