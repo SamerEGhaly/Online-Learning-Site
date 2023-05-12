084fc9a1c9d4376f16225f56fcc2cd5438814718
Samer Ghaly <samereaghaly@gmail.com>
2023-05-12 14:09:32 +0300

    organized and optimized the certifications component

--- src/Components/CertificationCourses.jsx
+++ src/Components/CertificationCourses.jsx
@@ -27,94 +27,7 @@ function CertificationCourses(){
     })
   }
 
-  function getCheckedBoxes(){ // loops over the booleanFilters state and returns an array of checked boxes names
-    var checkedBoxes = []
-    for (const key in booleanFilters) {
-      if (Object.hasOwnProperty.call(booleanFilters, key)) {
-        if(booleanFilters[key]){
-          checkedBoxes.push(key)
-        }
-      }
-    }
-    return checkedBoxes
-  }
-
-  function sortCourses(){ // sorts the carouselItems array according to the sortValue state
-    if(sortValue == "highestRating"){
-      for(let i = 0; i < carouselItems.length; i++){
-        for(let j = 0, k = 1; k < carouselItems.length - i; j++, k++){
-          var currentRating = parseFloat(carouselItems[j].props.stars)
-          var nextRating = parseFloat(carouselItems[k].props.stars)
-          if(currentRating < nextRating){
-            var temp = carouselItems[j]
-            carouselItems[j] = carouselItems[k]
-            carouselItems[k] = temp
-          }
-        }
-      }
-    }
-    else if(sortValue == "lowestRating"){
-      for(let i = 0; i < carouselItems.length; i++){
-        for(let j = 0, k = 1; k < carouselItems.length - i; j++, k++){
-          var currentRating = parseFloat(carouselItems[j].props.stars)
-          var nextRating = parseFloat(carouselItems[k].props.stars)
-          if(currentRating > nextRating){
-            var temp = carouselItems[j]
-            carouselItems[j] = carouselItems[k]
-            carouselItems[k] = temp
-          }
-        }
-      }
-    }
-  }
-
-  function getCarouselItems(){  // returns an array of items to be passed to the carousel component
-    var carouselItems = []
-    var showCourse = false
-    var item
-    for(let i = 0; i < coursesDatabase.length; i++){
-  
-      showCourse = false
-      item = coursesDatabase[i]
-  
-      if(checkedBoxes.length > 0){
-        if(booleanFilters["freeCertificationsFilter"] && item.price == "free"){
-          showCourse = true;
-        }
-        else if(booleanFilters["paidCertificationsFilter"] && item.price !="free"){
-          showCourse = true;
-        }
-      }
-      else {
-        showCourse = true;
-      }
-  
-      if(showCourse){
-        carouselItems.push(
-        <Course 
-          key={item.id}
-          title={item.title}
-          duration={item.duration}
-          price={item.price}
-          platform={item.platform}
-          stars={item.stars}
-          no_of_ratings={item.no_of_ratings}
-          tag={item.tag}
-          thumbnail={item.thumbnail}
-          certified={item.certified}
-        />
-        )
-      }
-    } // end of for loop
-  
-    return(carouselItems)
-  }
-  
-  const checkedBoxes = getCheckedBoxes() // an array of checked boxes names
-
-  const carouselItems = getCarouselItems()
-
-  sortCourses()
+  const carouselItems = getCarouselItems(sortValue, getCheckedBoxes(booleanFilters), booleanFilters)
 
   return(
       <section className="certification-courses">
@@ -147,3 +60,130 @@ function CertificationCourses(){
 }
 
 export default CertificationCourses
+
+/*
+  description: search for boolean filters that are checked or "true"
+
+  inputs:
+    booleanFilters: an object containing boolean filters and there values (true or false)
+  
+  return: an array of names of true boolean filters
+*/
+function getCheckedBoxes(booleanFilters){
+  var checkedBoxes = []
+  for (const key in booleanFilters) {
+    if (Object.hasOwnProperty.call(booleanFilters, key)) {
+      if(booleanFilters[key]){
+        checkedBoxes.push(key)
+      }
+    }
+  }
+  return checkedBoxes
+}
+
+
+/*
+  description: sorts the carouselItems array according to the sortValue state
+
+  inputs:
+    sortValue: value to sort the array by it
+    carouselItems: an array of the items to be sorted
+
+  return: none
+*/
+function sortItems(sortValue, carouselItems){
+  if(sortValue == "highestRating"){
+    for(let i = 0; i < carouselItems.length; i++){
+      for(let j = 0, k = 1; k < carouselItems.length - i; j++, k++){
+        var currentRating = parseFloat(carouselItems[j].props.stars)
+        var nextRating = parseFloat(carouselItems[k].props.stars)
+        if(currentRating < nextRating){
+          var temp = carouselItems[j]
+          carouselItems[j] = carouselItems[k]
+          carouselItems[k] = temp
+        }
+      }
+    }
+  }
+  else if(sortValue == "lowestRating"){
+    for(let i = 0; i < carouselItems.length; i++){
+      for(let j = 0, k = 1; k < carouselItems.length - i; j++, k++){
+        var currentRating = parseFloat(carouselItems[j].props.stars)
+        var nextRating = parseFloat(carouselItems[k].props.stars)
+        if(currentRating > nextRating){
+          var temp = carouselItems[j]
+          carouselItems[j] = carouselItems[k]
+          carouselItems[k] = temp
+        }
+      }
+    }
+  }
+}
+
+/*
+  description: decides whether an item passes the filter to be shown on the carousel or not.
+
+  takes three inputs: 
+    booleanFilters: an object containing boolean filters and there values (true or false)
+    checkedBoxes: an array holding the names of boolean filters that are checked or "true"
+    item: the item to be filtered
+    
+  returns: true if the item passes the filters and false otherwise.
+*/
+function filterItem(booleanFilters, checkedBoxes, item){
+  if(checkedBoxes.length > 0){
+    if(booleanFilters["freeCertificationsFilter"] && item.price == "free"){
+      return(true)
+    }
+    else if(booleanFilters["paidCertificationsFilter"] && item.price !="free"){
+      return(true)
+    }
+    else{
+      return(false)
+    }
+  }
+  else {
+    return(true)
+  }
+}
+
+/*
+  description: gets a sorted and filtered array of items to be passed to the carousel component
+
+  inputs:
+    sortValue: sortValue: value to sort the array by it
+    checkedBoxes: an array holding the names of boolean filters that are checked or "true"
+    booleanFilters: an object containing boolean filters and there values (true or false)
+
+  return: an array of items passed to the carousel
+*/
+function getCarouselItems(sortValue, checkedBoxes, booleanFilters){
+  var carouselItems = []
+  var item
+
+  for(let i = 0; i < coursesDatabase.length; i++){
+
+    item = coursesDatabase[i]
+
+    if(filterItem(booleanFilters, checkedBoxes, item)){
+      carouselItems.push(
+      <Course 
+        key={item.id}
+        title={item.title}
+        duration={item.duration}
+        price={item.price}
+        platform={item.platform}
+        stars={item.stars}
+        no_of_ratings={item.no_of_ratings}
+        tag={item.tag}
+        thumbnail={item.thumbnail}
+        certified={item.certified}
+      />
+      )
+    }
+  } // end of for loop
+
+  sortItems(sortValue, carouselItems)
+
+  return(carouselItems)
+}
